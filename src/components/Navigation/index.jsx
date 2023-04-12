96ede9ced445054a8d8e9b4ce200709c0e33907c
antony2432 <achuquival@gmail.com>
2023-04-12 18:36:13 -0500

    creando el componente Navigation y dando estilos

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,13 +1,14 @@
 import { urlPath } from "./logica";
-// import SearchBar from "../SearchBar";
-import "./style.css"
+import "./style.css";
 
 export default function Navigation() {
   return (
     <nav className="navigation">
       <ul>
-        {urlPath.map(({ name, color }) => (
-          <li style={{"--hoverandborder": `${color}`}}><span>{name}</span></li>
+        {urlPath.map(({ name, color }, i) => (
+          <li key={i} style={{ "--hoverandborder": `${color}` }}>
+            <span>{name}</span>
+          </li>
         ))}
       </ul>
       {/* <SearchBar onSearch={(characterID) => window.alert(characterID)} /> */}
